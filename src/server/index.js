c03215cbb7eb0ccd2a31c0909a5df55f5676550a
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-06 15:53:41 +0100

    create user schema, controller and lib/account

--- src/server/index.js
+++ src/server/index.js
@@ -13,12 +13,57 @@ const {APP_PORT} = process.env;
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+// Connexion à la base de donnée
+mongoose.connect("mongodb://dev:dev@mongo:27017/Bookshelf?authSource=admin");
+
+// .then(() => {
+//     console.log('Connected to mongoDB')
+// }).catch(e => {
+//     console.log('Error while DB connecting');
+//     console.log(e);
+// });
+
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "db connection erro"));
+db.once("open", () => {
+    console.log("connected");
+});
+
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
-app.get("/hello", (req, res) => {
-    console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
-    res.send("Hello, World!");
+// Body Parser
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true,
 });
+
+app.use(urlencodedParser);
+app.use(bodyParser.json());
+
+// Définition des CORS
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type",
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+// Définition du routeur
+const router = new express.Router();
+
+app.use("/user", router);
+require(`${__dirname}/controllers/userController`)(router);
 
 app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
