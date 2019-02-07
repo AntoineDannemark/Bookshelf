78c396fcef809fc60520c40e08575533ce7a7115
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-07 14:30:52 +0100

    fetch all users OK

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,15 @@
+const User = require("../schema/schemaUser");
+const account = require("./account/lib");
+
+module.exports = function(app) {
+    app.post("/login", account.login);
+    app.post("/signup", account.signup);
+    app.get("/", (req, res) => {
+        User.find({}, (err, users) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send({users: users});
+        });
+    });
+};
--- src/server/controllers/usersController.js
+++
@@ -1,9 +0,0 @@
-const account = require("./account/lib");
-
-module.exports = function(app) {
-    app.post("/login", account.login);
-    app.post("/signup", account.signup);
-    // app.get("/", function(req, res) {
-    //     res.send()
-    // });
-};
