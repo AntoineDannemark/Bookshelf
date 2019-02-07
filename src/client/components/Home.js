b9df9c432808102a89cf8df3c64ee98f3d1a1c69
RogerNoel <noel.roger.pro@gmail.com>
2019-02-07 20:28:33 +0100

    achevement page accueil

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,22 +1,50 @@
 import React, {Component} from "react";
 import {Route, Link} from "react-router-dom";
-import '../../styles/style.css';
+import "../../styles/style.css";
 
 export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    //   handleChange(event) {
+    //     this.setState({value: event.target.value});
+    //   }
+    
+    //   handleSubmit(event) {
+    //     alert('Submitted: ' + this.state.value);
+    //     event.preventDefault();
+    //   }
     render() {
         return (
-            <div className='main'>
-                <img src={require("../../styles/images/bshelf2.png")} />
+            <div className="main">
+                <div className='logo'>
+                    <img src={require("../../styles/images/bshelf2.png")} />
+                </div>
+
                 <div>
                     <p className="library">
                         Your <span className="blue">code</span> library
                     </p>
                 </div>
-                <div id="signbuttonscontainer">
-                    <button className="signbuttons" type="button">
-                        Sign In
-                    </button>
+
+                <div className='logformbackground'>
+                    <form className='logform' onSubmit={this.handleSubmit}>
+                        <div>
+                            <input type="text" placeholder='Email' />
+                        </div>
+                        <div>
+                            <input type="text" placeholder= 'Password' />
+                        </div>
+                        <div className='positionrelative'>
+                            <input className='logsubmitbutton' type="submit" value="Submit" />
+                        </div>
+                    </form>
                 </div>
+
             </div>
         );
     }
