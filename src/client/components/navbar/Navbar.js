0ffc1f7e916d0a838fa0d7667f71e34012891d26
RogerNoel <noel.roger.pro@gmail.com>
2019-02-13 13:22:52 +0100

    structure navbar ok

--- src/client/components/navbar/Navbar.js
+++ src/client/components/navbar/Navbar.js
@@ -5,6 +5,9 @@ import Search from "../pages/Searchpage";
 import Review from "../pages/ReviewPage";
 import Adduser from "../pages/Newuserpage";
 import Removeuser from "../pages/RemoveUserPage";
+import Movement from "../pages/BookMovementPage";
+import Addbook from "../pages/AddBookPage";
+import Removebook from "../pages/RemoveBookPage";
 
 class Navbar extends Component {
     render() {
@@ -25,12 +28,24 @@ class Navbar extends Component {
                             <Link to="removeuser"> {"Delete user"} </Link>
                         </li>
                     </ul>
+                    <li>
+                        <Link to="/movement">{"Mouvements"}</Link>
+                    </li>
+                    <li>
+                        <Link to="/addbook">{"Add book"}</Link>
+                    </li>
+                    <li>
+                        <Link to="/removebook">{"Delete book"}</Link>
+                    </li>
                 </ul>
 
                 <Route path="/search" Component={Search} />
                 <Route path="/review" Component={Review} />
                 <Route path="/newuser" Component={Adduser} />
                 <Route path="/removeuser" Component={Removeuser} />
+                <Route path="/movement" Component={Movement} />
+                <Route path="/addbook" Component={Addbook} />
+                <Route path="/removebook" Component={Removebook} />
             </div>
         );
     }
