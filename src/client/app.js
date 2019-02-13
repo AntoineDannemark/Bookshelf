48eacaaf136047037a4df17a70b9e538534ed2c0
RogerNoel <noel.roger.pro@gmail.com>
2019-02-13 10:33:46 +0100

    fix app.js apres merge

--- src/client/app.js
+++ src/client/app.js
@@ -5,30 +5,9 @@ import Login from "./components/Login";
 import EnteteAddBook from "./components/EnteteAddBook";
 import NotFound from "./components/NotFound";
 
-<<<<<<< HEAD
-import AddBookPage from "./components/pages/AddBookPage";
-import BookMovementPage from "./components/pages/BookMovementPage";
-import Home from "./components/Home";
-import Newuserpage from "./components/pages/Newuserpage";
-import RemoveBookPage from "./components/page/RemoveBookPage";
-import RemoveUserPage from "./components/RemoveUserPage";
-import ReviewPage from "./components/ReviewPage";
-import Searchpage from "./components/Searchpage";
-
-class Routing extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         useremail: "",
-    //         userpassword: "",
-    //     };
-    //     this.handleClick = this.handleClick.bind(this);
-    // }
-=======
 import "../styles/style.css";
 
 class App extends Component {
->>>>>>> pull_antoine
     render() {
         return (
             <div className="App">
